f998ecbb2dcc124e6ca2321ee657e3fbd9a727cc
Kundan <131183962+CodeBlocker52@users.noreply.github.com>
2024-07-21 22:06:32 +0530

    Create Token.sol

---
+++ Token.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
+        _mint(msg.sender, initialSupply);
+    }
+}
+
+
+// Contract deployed at : 0xDAE85D8Bc077e6E910e2ADbb050F719D53d093D7
